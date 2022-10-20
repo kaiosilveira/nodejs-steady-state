e9579fcf259040816fd31b489f8f3ba5b48a531d
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-20 21:21:11 +0100

    add mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import http from 'http';
 import { createClient } from 'redis';
+import mongoose from 'mongoose';
 import ExpressAppFactory from './presentation/express';
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 8080;
 const app = ExpressAppFactory.createApp();
 
 http.createServer(app).listen(PORT, async () => {
   console.log(`server listening at ${PORT} 🚀`);
 
-  setTimeout(async () => {
-    const client = createClient({ url: 'redis://redis:6379' });
-    client.on('error', err => console.log('Redis Client Error', err));
+  const redisClient = createClient({ url: 'redis://redis:6379' });
+  redisClient.on('error', err => console.log('Redis Client Error', err));
 
-    await client.connect();
+  await redisClient.connect();
+  console.log('Redis connected');
 
-    await client.set('key', 'value');
-    const value = await client.get('key');
-
-    console.log(`value retrieved from redis cache: ${value}`);
-  }, 5000);
+  const mongodbConn = mongoose.createConnection('mongodb://mongodb');
+  mongodbConn.on('connected', () => console.log('MongoDB connected'));
 });
