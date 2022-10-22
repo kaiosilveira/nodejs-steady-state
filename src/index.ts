b91be846730e7fa2bfc3c2bfe3b78a8058a5fd6f
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 12:48:25 +0100

    replace console.log by managed logger calls

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,24 @@ import http from 'http';
 import * as Redis from 'redis';
 import Mongoose from 'mongoose';
 import ExpressAppFactory from './presentation/express';
+import ConsoleLogger from './application/observability/logger/console';
 
 const PORT = Number(process.env.PORT) || 8080;
 const app = ExpressAppFactory.createApp();
 
 http.createServer(app.instance).listen(PORT, async () => {
-  console.log(`server listening at ${PORT} 🚀`);
+  const logger = new ConsoleLogger();
+
+  logger.info({ message: `server listening at ${PORT} 🚀` });
 
   const redisClient = Redis.createClient({ url: 'redis://redis:6379' });
-  redisClient.on('error', err => console.log('Redis Client Error', err));
+  redisClient.on('error', (err: Error) =>
+    logger.info({ message: 'Redis Client Error', errMsg: err.message, stack: err.stack })
+  );
 
   await redisClient.connect();
-  console.log('Redis connected');
+  logger.info('Redis connected');
 
   const mongodbConn = Mongoose.createConnection('mongodb://mongodb');
-  mongodbConn.on('connected', () => console.log('MongoDB connected'));
+  mongodbConn.on('connected', () => logger.info({ message: 'MongoDB connected' }));
 });
