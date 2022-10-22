965c006d676da1cf040b4f84993b99d6880a162e
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 13:03:54 +0100

    change ManagedRedisClient tests to use jest functions

--- src/data-access/in-memory/redis/client/index.spec.ts
+++ src/data-access/in-memory/redis/client/index.spec.ts
@@ -1,40 +1,42 @@
 import { ManagedRedisClient, RedisClient } from '.';
 import Logger from '../../../../application/observability/logger';
 
-const noop = () => {};
-
 describe('ManagedRedisClient', () => {
   const logger = { error: jest.fn() } as unknown as Logger;
+  const fakeLRangeFn = jest.fn();
+  const fakeLPushFn = jest.fn();
+  const fakeExpireAtFn = jest.fn();
+
   const fakeRedisClient = {
-    get: noop,
-    set: noop,
-    lPush: noop,
-    lRange: noop,
-    expireAt: noop,
-    connect: noop,
-    disconnect: noop,
-    on: noop,
+    get: jest.fn(),
+    set: jest.fn(),
+    connect: jest.fn(),
+    disconnect: jest.fn(),
+    on: jest.fn(),
+    lPush: fakeLPushFn,
+    expireAt: fakeExpireAtFn,
+    lRange: fakeLRangeFn,
   } as unknown as RedisClient;
+
+  afterEach(() => {
+    fakeLRangeFn.mockReset();
+    fakeLPushFn.mockReset();
+    fakeExpireAtFn.mockReset();
+  });
 
   describe('connect', () => {
     it('should initiate a connection with the Redis server', async () => {
-      const spyOnConnect = jest.spyOn(fakeRedisClient, 'connect');
-
       const db = new ManagedRedisClient({ logger, redisClient: fakeRedisClient });
       await db.connect();
-
-      expect(spyOnConnect).toHaveBeenCalledTimes(1);
+      expect(fakeRedisClient.connect).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('disconnect', () => {
     it('should disconnect from the Redis server', async () => {
-      const spyOnDisconnect = jest.spyOn(fakeRedisClient, 'disconnect');
-
       const db = new ManagedRedisClient({ logger, redisClient: fakeRedisClient });
       await db.disconnect();
-
-      expect(spyOnDisconnect).toHaveBeenCalledTimes(1);
+      expect(fakeRedisClient.disconnect).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -48,14 +50,13 @@ describe('ManagedRedisClient', () => {
 
     it('should set a new key with the specified expire time', async () => {
       const EXPIRE_TIME_IN_SECONDS = 60;
-      const spyOnSet = jest.spyOn(fakeRedisClient, 'set').mockResolvedValue(null);
       const key = 'my:key';
       const value = { key: 'value' };
 
       const db = new ManagedRedisClient({ logger, redisClient: fakeRedisClient });
       await db.set(key, value, EXPIRE_TIME_IN_SECONDS);
 
-      expect(spyOnSet).toHaveBeenCalledWith(key, JSON.stringify(value), {
+      expect(fakeRedisClient.set).toHaveBeenCalledWith(key, JSON.stringify(value), {
         EX: EXPIRE_TIME_IN_SECONDS,
       });
     });
@@ -100,17 +101,15 @@ describe('ManagedRedisClient', () => {
     it('should create a key containing a list of items with a specified expire time', async () => {
       const EXPIRE_TIME_IN_SECONDS = 60;
       const key = 'my:key';
-      const spyOnLPush = jest.spyOn(fakeRedisClient, 'lPush');
-      const spyOnExpireAt = jest.spyOn(fakeRedisClient, 'expireAt');
       const db = new ManagedRedisClient({ logger, redisClient: fakeRedisClient });
 
       await db.addToList(key, EXPIRE_TIME_IN_SECONDS, { key: 'value1' }, { key: 'value2' });
 
-      expect(spyOnLPush).toHaveBeenCalledWith(key, [
+      expect(fakeLPushFn).toHaveBeenCalledWith(key, [
         JSON.stringify({ key: 'value1' }),
         JSON.stringify({ key: 'value2' }),
       ]);
-      expect(spyOnExpireAt).toHaveBeenCalledWith(key, EXPIRE_TIME_IN_SECONDS);
+      expect(fakeExpireAtFn).toHaveBeenCalledWith(key, EXPIRE_TIME_IN_SECONDS);
     });
   });
 
@@ -124,13 +123,13 @@ describe('ManagedRedisClient', () => {
 
     it('should retrieve all items for a given list', async () => {
       const storedData = [JSON.stringify({ key: 'value1' }), JSON.stringify({ key: 'value2' })];
-      const spyOnLRange = jest.spyOn(fakeRedisClient, 'lRange').mockResolvedValue(storedData);
+      fakeLRangeFn.mockResolvedValue(storedData);
 
       const db = new ManagedRedisClient({ logger, redisClient: fakeRedisClient });
       const result = await db.getList('my:key');
 
       expect(result).toEqual(storedData.map(d => JSON.parse(d)));
-      expect(spyOnLRange).toHaveBeenCalledWith('my:key', 0, -1);
+      expect(fakeLRangeFn).toHaveBeenCalledWith('my:key', 0, -1);
     });
   });
 });
