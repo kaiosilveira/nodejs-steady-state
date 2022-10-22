d584fe99655e08b1d73ed82cc808f4368edc6da8
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 14:06:57 +0100

    change ExpressAppFactory to receive appState as a parameter

--- src/presentation/express/index.ts
+++ src/presentation/express/index.ts
@@ -2,11 +2,11 @@ import Express from 'express';
 import Helmet from 'helmet';
 import Mongoose from 'mongoose';
 import InMemoryDatabase from '../../data-access/in-memory';
-
+import ApplicationState from '../application-state';
 import PresentationResourcesManager from '../resources';
 
 export default class ExpressAppFactory {
-  static createApp() {
+  static createApp({ appState }: { appState: ApplicationState }) {
     const instance = Express();
     let inMemoryDatabaseClient: InMemoryDatabase;
     let diskDatabaseConnection: Mongoose.Connection;
@@ -14,7 +14,9 @@ export default class ExpressAppFactory {
     instance.use(Helmet());
     instance.use(Express.json());
     instance.use(Express.urlencoded({ extended: true }));
-    instance.use(PresentationResourcesManager.configureRouter(Express.Router()));
+    instance.use(
+      PresentationResourcesManager.configureRouter({ appState, router: Express.Router() })
+    );
 
     return {
       instance,
