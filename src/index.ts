d4295a6104d9261841878a8de077edb3197994d8
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 13:29:23 +0100

    remove log regarding redis connection at index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as Redis from 'redis';
 import Mongoose from 'mongoose';
 import ExpressAppFactory from './presentation/express';
 import ConsoleLogger from './application/observability/logger/console';
+import { ManagedRedisClient } from './data-access/in-memory/redis/client';
 
 const PORT = Number(process.env.PORT) || 8080;
 const app = ExpressAppFactory.createApp();
@@ -12,13 +13,12 @@ http.createServer(app.instance).listen(PORT, async () => {
 
   logger.info({ message: `server listening at ${PORT} 🚀` });
 
-  const redisClient = Redis.createClient({ url: 'redis://redis:6379' });
-  redisClient.on('error', (err: Error) =>
-    logger.info({ message: 'Redis Client Error', errMsg: err.message, stack: err.stack })
-  );
+  const redisClient = new ManagedRedisClient({
+    logger,
+    redisClient: Redis.createClient({ url: 'redis://redis:6379' }),
+  });
 
   await redisClient.connect();
-  logger.info('Redis connected');
 
   const mongodbConn = Mongoose.createConnection('mongodb://mongodb');
   mongodbConn.on('connected', () => logger.info({ message: 'MongoDB connected' }));
