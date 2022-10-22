12732c5688e91dad0f0b780dc923c5842ec5bdb6
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 16:36:24 +0100

    update FakeExpressFactory to provide default values for req props

--- src/__mocks__/express/factory/index.ts
+++ src/__mocks__/express/factory/index.ts
@@ -2,8 +2,20 @@ import { Request, Response } from 'express';
 import FakeExpressResponse from '../response';
 
 export default class FakeExpressFactory {
-  static createRequest({ headers, body, ctx } = { headers: {}, body: {}, ctx: {} }): Request {
-    return { headers, body, context: ctx } as unknown as Request;
+  static createRequest(
+    {
+      headers,
+      body,
+      params,
+      ctx,
+    }: { headers?: object; body?: object; params?: object; ctx?: object } = {
+      headers: {},
+      body: {},
+      params: {},
+      ctx: {},
+    }
+  ): Request {
+    return { headers, body, params, context: ctx } as unknown as Request;
   }
 
   static createResponse(): Response {
