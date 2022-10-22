85b78d9fbb883d6bf850d1be39cca3ac591bd6be
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 16:37:08 +0100

    attach RealtimeGeolocationController to a route

--- src/presentation/resources/index.ts
+++ src/presentation/resources/index.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import InMemoryDatabase from '../../data-access/in-memory';
 import ApplicationState from '../application-state';
 import HealthCheckController, { ApplicationEnv } from './health-check/controller';
+import RealtimeGeolocationController from './realtime-geolocation/controller';
 
 export default class PresentationResourcesManager {
-  static configureRouter({ appState, router }: { appState: ApplicationState; router: Router }) {
+  static configureRouter({
+    inMemoryDatabaseClient,
+    appState,
+    router,
+  }: {
+    inMemoryDatabaseClient: InMemoryDatabase;
+    appState: ApplicationState;
+    router: Router;
+  }) {
     const env: ApplicationEnv = {
       NODE_ENV: process.env.NODE_ENV || 'dev',
       COMMIT_SHA: process.env.COMMIT_SHA || 'unknown',
@@ -11,8 +21,12 @@ export default class PresentationResourcesManager {
     };
 
     const healthCheckCtrl = new HealthCheckController({ applicationState: appState, env });
+    const realtimeGeolocationCtrl = new RealtimeGeolocationController({ inMemoryDatabaseClient });
 
     router.get('/health', healthCheckCtrl.getHealthState);
+    router.post('/geo/:itemId', realtimeGeolocationCtrl.processGeolocationInfo);
+    router.get('/geo/:itemId/latest', realtimeGeolocationCtrl.getLatestGeolocationInfo);
+
     return router;
   }
 }
