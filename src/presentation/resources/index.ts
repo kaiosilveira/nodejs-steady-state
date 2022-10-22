02fd3ecf2365f87b0bffa967a833d9358d9a7712
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 14:06:39 +0100

    change PresentationResourcesManager to receive appState as a parameter

--- src/presentation/resources/index.ts
+++ src/presentation/resources/index.ts
@@ -1,21 +1,18 @@
 import { Router } from 'express';
-import InMemoryApplicationState from '../application-state/in-memory';
+import ApplicationState from '../application-state';
 import HealthCheckController, { ApplicationEnv } from './health-check/controller';
 
 export default class PresentationResourcesManager {
-  static configureRouter(router: Router) {
-    const applicationState = new InMemoryApplicationState();
+  static configureRouter({ appState, router }: { appState: ApplicationState; router: Router }) {
     const env: ApplicationEnv = {
       NODE_ENV: process.env.NODE_ENV || 'dev',
       COMMIT_SHA: process.env.COMMIT_SHA || 'unknown',
       NODE_VERSION: process.version,
     };
 
-    const healthCheckCtrl = new HealthCheckController({ applicationState, env });
+    const healthCheckCtrl = new HealthCheckController({ applicationState: appState, env });
 
     router.get('/health', healthCheckCtrl.getHealthState);
-
-    applicationState.setReady(true);
     return router;
   }
 }
