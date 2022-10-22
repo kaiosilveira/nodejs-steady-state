0e96279527b8ac086feb35c031ceb50ce092ee3e
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 14:06:11 +0100

    add app ready state info at ConsoleLogger

--- src/application/observability/logger/console/index.ts
+++ src/application/observability/logger/console/index.ts
@@ -1,13 +1,26 @@
 import Logger from '..';
+import ApplicationState from '../../../../presentation/application-state';
 
 export default class ConsoleLogger implements Logger {
+  private readonly _appState: ApplicationState;
+
+  constructor({ appState }: { appState: ApplicationState }) {
+    this._appState = appState;
+  }
+
   info(args: Object): void {
-    console.log(JSON.stringify({ ...args, at: new Date().toISOString() }));
+    console.log(
+      JSON.stringify({ ...args, at: new Date().toISOString(), appReady: this._appState.isReady() })
+    );
   }
   warn(args: Object): void {
-    console.warn(JSON.stringify({ ...args, at: new Date().toISOString() }));
+    console.warn(
+      JSON.stringify({ ...args, at: new Date().toISOString(), appReady: this._appState.isReady() })
+    );
   }
   error(args: Object): void {
-    console.error(JSON.stringify({ ...args, at: new Date().toISOString() }));
+    console.error(
+      JSON.stringify({ ...args, at: new Date().toISOString(), appReady: this._appState.isReady() })
+    );
   }
 }
