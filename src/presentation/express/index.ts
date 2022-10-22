1c51a9ce6ca1e2e04d67c1dd093f134f78f68ee1
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 13:54:26 +0100

    add methods to attach in-memory and disk dbs to app

--- src/presentation/express/index.ts
+++ src/presentation/express/index.ts
@@ -1,17 +1,29 @@
 import Express from 'express';
 import Helmet from 'helmet';
+import Mongoose from 'mongoose';
+import InMemoryDatabase from '../../data-access/in-memory';
 
 import PresentationResourcesManager from '../resources';
 
 export default class ExpressAppFactory {
   static createApp() {
     const instance = Express();
+    let inMemoryDatabaseClient: InMemoryDatabase;
+    let diskDatabaseConnection: Mongoose.Connection;
 
     instance.use(Helmet());
     instance.use(Express.json());
     instance.use(Express.urlencoded({ extended: true }));
     instance.use(PresentationResourcesManager.configureRouter(Express.Router()));
 
-    return { instance };
+    return {
+      instance,
+      attachInMemoryDbClient: (client: InMemoryDatabase) => {
+        inMemoryDatabaseClient = client;
+      },
+      attachDiskDatabaseConnection: (conn: Mongoose.Connection) => {
+        diskDatabaseConnection = conn;
+      },
+    };
   }
 }
