203677587e09ddb88b5c81dca9371e7e57452e3b
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 21:57:00 +0100

    add execTransaction method to InMemoryDatabase

--- src/data-access/in-memory/index.ts
+++ src/data-access/in-memory/index.ts
@@ -3,4 +3,11 @@ export default interface InMemoryDatabase {
   set(key: string, value: Object, expireTimeInSeconds: number): Promise<void>;
   addToList(key: string, ...values: Array<Object>): Promise<void>;
   getList(key: string): Promise<Object[]>;
+  execTransaction({
+    key,
+    transactionBlock,
+  }: {
+    key: string;
+    transactionBlock: Function;
+  }): Promise<void>;
 }
