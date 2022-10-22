cf4eb66c230f4ea08a5ed9e9cc81780382061cc0
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-22 21:57:18 +0100

    add dummy execTransaction method to FakeInMemoryDatabase

--- src/data-access/in-memory/fake/index.ts
+++ src/data-access/in-memory/fake/index.ts
@@ -1,15 +1,28 @@
 import InMemoryDatabase from '..';
 
 export default class FakeInMemoryDatabase implements InMemoryDatabase {
+  execTransaction({
+    key,
+    transactionBlock,
+  }: {
+    key: string;
+    transactionBlock: Function;
+  }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
   get<T>(key: string): Promise<T | undefined> {
     throw new Error('Method not implemented.');
   }
+
   set(key: string, value: Object, expireTimeInSeconds: number): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   addToList(key: string, ...values: Object[]): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   getList(key: string): Promise<Object[]> {
     throw new Error('Method not implemented.');
   }
